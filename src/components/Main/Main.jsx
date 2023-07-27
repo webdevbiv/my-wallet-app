5f3ea8bdb164bc2aec22d4ac561e1f8a33b30dcd
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-07-27 23:00:40 +0300

    fix-margin-main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,10 @@
 import { Container } from "../Container/Container";
 import PropTypes from "prop-types";
+import s from "./Main.module.scss";
 const Main = ({ children }) => {
   return (
     <>
-      <main>
+      <main className={s.main}>
         <Container>{children}</Container>
       </main>
     </>
